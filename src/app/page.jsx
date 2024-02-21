3ed979e954009922c5a02eddb5e7994b61056d72
caelan-g <141709327+caelan-g@users.noreply.github.com>
2024-02-21 20:26:13 +1100

    working on it

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,27 +1,31 @@
 import Link from "next/link";
+import SideBar from "@/components/SideBar";
 
 export default function Home() {
   return (
-    <main className="grid h-screen place-items-center px-6 py-12 sm:py-32">
-      <div className="text-center">
-        <h1 className="mt-4 font-bold text-black dark:text-white text-5xl">
-          Choose a Team
-        </h1>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <Link
-            href={"/greybacks"}
-            className="rounded-md bg-gray-400 px-3 py-2 text-sm font-semibold text-white shadow-sm"
-          >
-            Greybacks
-          </Link>
-          <Link
-            href={"/redbacks"}
-            className="rounded-md bg-red-400 px-3 py-2 text-sm font-semibold text-white shadow-sm"
-          >
-            Redbacks
-          </Link>
+    <>
+      <SideBar />
+      <main className="grid h-screen place-items-center px-6 py-12 sm:py-32">
+        <div className="text-center">
+          <h1 className="mt-4 font-bold dark:text-white text-black text-5xl">
+            Choose a Team
+          </h1>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Link
+              href={"/greybacks"}
+              className="rounded-md bg-gray-400 px-3 py-2 text-sm font-semibold text-white shadow-sm"
+            >
+              Greybacks
+            </Link>
+            <Link
+              href={"/redbacks"}
+              className="rounded-md bg-red-400 px-3 py-2 text-sm font-semibold text-white shadow-sm"
+            >
+              Redbacks
+            </Link>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
